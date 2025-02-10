3d69f49364be74d7147d0d918695cbb3bbbb215f
Toqa <toqat490@gmail.com>
2025-02-10 22:40:34 +0200

    upload files

---
+++ js/log-in.js
@@ -0,0 +1,14 @@
+ const eye = document.getElementById('Password_eye');
+ const pass = document.getElementById('Password');
+ const icon =document .querySelector('.icon')
+ eye.addEventListener('click',function(){
+    if(pass .type==='pasword'){
+     pass.type='text';
+     icon .classList.remove('fa-eye');
+     icon.classList.add(' fa-eye-slash');
+    }else{
+        pass.type ='password';
+        icon .classList.add('fa-eye');
+        icon.classList.remove('fa-eye-slash');
+    }
+ })

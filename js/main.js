9e2fdedd0fd7ee2a0f140e49040cb22c78796f16
notsaaad <https://github.com/notsaaad/sawyancom.git>
2025-02-11 19:00:55 +0200

    update some product page managment

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 
 
+$(document).ready(function(){ // Every Password input add icon to show password when clicked
+  $('.password-with-icon input[type="password"]').after(`<span class="eye password_eye" > <i class="fa-solid fa-eye icon"></i></span>`);
+});
 
 
+    //Show Passowrd when Click on  Eye icon
     $(document).ready(function(){
-
-      let passwords = $('#Password_eye');
+      let passwords = $('.password_eye');
       passwords.each( function(i , ele) {
-        $(ele).on('click', ()=>{
+        $(ele).on('click', () =>{
           $(this).attr('type')
           let type =  $(this).siblings('input').attr('type');
-          type == 'password' ? $(this).siblings('input').attr('type', 'text') : $(this).siblings('input').attr('type', 'password');
+          type == 'password' ? $(this).siblings('input').attr('type', 'text')             : $(this).siblings('input').attr('type', 'password');
           type == 'password' ? $(this).html(`<i class="fa-solid fa-eye-slash icon"></i>`) : $(this).html(`<i class="fa-solid fa-eye icon"></i>`);
         })
       } )
-
-
-
     });
-  
+
+
